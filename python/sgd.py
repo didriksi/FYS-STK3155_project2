a91188d3452779a7f17231120dbfa936c25f09b1
Didrik Sten Ingebrigtsen <didrik@eduroam-193-157-246-13.wlan.uio.no>
2020-10-22 19:52:46 +0200

    Added option for several initial conditions in SGD, and put the errors in a dataframe

--- python/sgd.py
+++ python/sgd.py
@@ -1,11 +1,12 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 import tune
 import metrics
 import linear_models
 
-def sgd(data, mini_batches, epochs, learning_rate, polynomials, momentum, init_beta=lambda b: np.random.randn(b), metric=metrics.MSE):
+def sgd(data, mini_batches, epochs, learning_rate_iter, polynomials, momentum=0, _lambda=0, initial_conditions=1, metric=metrics.MSE):
     """Stochastic gradient descent
 
     Parameters:
@@ -17,77 +18,96 @@ def sgd(data, mini_batches, epochs, learning_rate, polynomials, momentum, init_b
                 Number of mini-batches.
     epochs:     int
                 Number of epochs.
-    learning_rate:
-                int -> float
-                Function that takes in a timestep, and returns a learning rate.
+    learning_rate_iter:
+                iterable (int -> float)
+                Iterable object of functions that takes in a timestep, and returns a learning rate.
     polynomials:int
                 Maximum degree of polynomials in design matrix
     momentum:   float
                 Gamma parameter, determining how much momentum to keep. Set to zero for standard SGD.
-    init_beta:  int b -> array of shape (b, )
-                Function that takes in the amount of beta parameters, and returns an initial beta array.
+    _lambda:    float
+                Shrinkage parameter for Ridge regression. Set to 0 for OLS.
+    initial_conditions:
+                int
+                Amount of attempts with different random initial conditions.
     metric:     array of shape(n, ), array of shape(n, ) -> float
                 Should take in y and y_tilde, and return the total error.
 
     
     Returns:
     --------
-    
+    errors:     pd.DataFrame
+                Dataframe with initial conditions and learning rates as multiindex, and epochs as column
     """
+
+    errors_index = pd.MultiIndex.from_product([
+        range(initial_conditions),
+        range(len(learning_rate_iter))],
+        names = ['Initial condition number', 'Learning rate number']
+        )
+    errors = pd.DataFrame(dtype=float, index=errors_index, columns=range(1, epochs + 1))
+    errors.sort_index(inplace=True)
+    print(errors)
 
     data_size = data['x_train'].shape[0]
     assert data_size%mini_batches == 0, "Data must be divisible with mini_batches"
 
     step = 0
-    errors = np.empty(epochs)
     X = tune.poly_design_matrix(polynomials, data['x_train'])
 
-    beta = init_beta(X.shape[1])
-    v = np.zeros_like(beta)
+    init_betas = np.random.randn(initial_conditions, X.shape[1])
+    for i, beta in enumerate(init_betas): # TODO: Vectorise this loop away
+        for j, learning_rate in enumerate(learning_rate_iters): # TODO: Vectorise this loop away
+            for epoch in range(1, epochs + 1):
+                print('epoch: ', epoch)
+                v = np.zeros_like(beta)
+                indexes_used = np.zeros(data_size, dtype=bool)
+                for mini_batch in range(1, mini_batches + 1):
+                    k = np.random.choice(np.arange(data_size)[indexes_used == False], size=(int(data_size/mini_batches)))
+                    indexes_used[k] = True
 
-    for epoch in range(1, epochs + 1):
-        print('epoch: ', epoch)
-        indexes_used = np.zeros(data_size, dtype=bool)
-        for mini_batch in range(1, mini_batches + 1):
-            k = np.random.choice(np.arange(data_size)[indexes_used == False], size=(int(data_size/mini_batches)))
-            indexes_used[k] = True
+                    y_tilde = X[k] @ beta
 
-            y_tilde = X[k] @ beta
+                    # TODO: Implement classes in linear_models.py in a way where this can come from it
+                    cost_diff = np.dot(X[k].T, 2/(data_size/mini_batches)*(y_tilde - data['y_train'][k])) + 2*_lambda*beta
 
-            cost_diff = np.dot(X[k].T, 2/(data_size/mini_batches)*(y_tilde - data['y_train'][k]))
+                    v = momentum*v + learning_rate(step)*cost_diff
+                    beta = beta - v
 
-            v = momentum * v + learning_rate(step)*cost_diff
-            beta = beta - v
+                    step += 1
 
-            step += 1
+                errors.loc[i,j][epoch] = metrics.MSE(X @ beta, data['y_train'])
 
-        errors[epoch-1] = metrics.MSE(X @ beta, data['y_train'])
-
-    return beta, errors
-
+    return errors
 
 if __name__ == '__main__':
     import real_terrain
 
     data = real_terrain.get_data(20)
-    print(data['x_train'].shape)
 
-    epochs = 1000
+    epochs = 20
     X_test = tune.poly_design_matrix(6, data['x_train'])
 
-    for i, learning_rate in enumerate(np.logspace(-7, -0.8, 5)):
-        beta, errors = sgd(data, 20, epochs, lambda t: learning_rate, 6, 0.5)
-        plt.semilogy(errors, label=f"$\\gamma$={learning_rate:.3e}")
-        print(f"{learning_rate}:\n Error: {metrics.MSE(data['y_train'], X_test @ beta)}\n beta: {beta}")
+    # TODO: Make class or some other datastructure for learning rate functions, so that it has a name?
+    learning_rate_iters = [lambda t: learning_rate for learning_rate in np.logspace(-4, -1, 4)]
+    errors = sgd(data, 20, epochs, learning_rate_iters, 6, momentum=0.5, _lambda=0.01, initial_conditions=5)
+
+    for j in range(len(learning_rate_iters)):
+        error_for_learning_rate = errors.xs(j, level=1, drop_level=False)
+        min_errors = error_for_learning_rate.min(axis=0)
+        max_errors = error_for_learning_rate.max(axis=0)
+        best_init_condition = error_for_learning_rate[epochs].idxmin()
+        best_errors = error_for_learning_rate.loc[best_init_condition]
+        # TODO: Make plot with min_errors and max_errors as extremes, and best_errors as center.
     
     model = linear_models.LinearRegression()
     model.fit(X_test, data['y_train'])
-
     plt.plot([0, epochs], np.repeat(metrics.MSE(model.predict(X_test), data['y_train']), 2), label="Analytical")
     plt.xlabel("Epoch")
     plt.ylabel("MSE")
+    plt.yscale("log")
     plt.legend()
     plt.savefig("../plots/sgd_different_learning_rates")
-    plt.show()
+    #plt.show()
 
 
