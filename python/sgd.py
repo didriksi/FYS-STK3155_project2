a551cc46c9c13333ef6ac99418fb88b30d95e429
Didrik Sten Ingebrigtsen <didrik@eduroam-193-157-246-13.wlan.uio.no>
2020-10-22 14:33:42 +0200

    Added momentum to stochastic gradient descent function

--- python/sgd.py
+++ python/sgd.py
@@ -5,7 +5,7 @@ import tune
 import metrics
 import linear_models
 
-def sgd(data, mini_batches, epochs, learning_rate, polynomials, init_beta=lambda b: np.random.randn(b), metric=metrics.MSE):
+def sgd(data, mini_batches, epochs, learning_rate, polynomials, momentum, init_beta=lambda b: np.random.randn(b), metric=metrics.MSE):
     """Stochastic gradient descent
 
     Parameters:
@@ -22,6 +22,8 @@ def sgd(data, mini_batches, epochs, learning_rate, polynomials, init_beta=lambda
                 Function that takes in a timestep, and returns a learning rate.
     polynomials:int
                 Maximum degree of polynomials in design matrix
+    momentum:   float
+                Gamma parameter, determining how much momentum to keep. Set to zero for standard SGD.
     init_beta:  int b -> array of shape (b, )
                 Function that takes in the amount of beta parameters, and returns an initial beta array.
     metric:     array of shape(n, ), array of shape(n, ) -> float
@@ -35,12 +37,14 @@ def sgd(data, mini_batches, epochs, learning_rate, polynomials, init_beta=lambda
 
     data_size = data['x_train'].shape[0]
     assert data_size%mini_batches == 0, "Data must be divisible with mini_batches"
-    #beta = init_beta(int(polynomials*(polynomials + 1)/2))
-    beta = init_beta(28)
-    step = 0
 
+    step = 0
     errors = np.empty(epochs)
     X = tune.poly_design_matrix(polynomials, data['x_train'])
+
+    beta = init_beta(X.shape[1])
+    v = np.zeros_like(beta)
+
     for epoch in range(1, epochs + 1):
         print('epoch: ', epoch)
         indexes_used = np.zeros(data_size, dtype=bool)
@@ -52,7 +56,8 @@ def sgd(data, mini_batches, epochs, learning_rate, polynomials, init_beta=lambda
 
             cost_diff = np.dot(X[k].T, 2/(data_size/mini_batches)*(y_tilde - data['y_train'][k]))
 
-            beta = beta - learning_rate(step)*cost_diff
+            v = momentum * v + learning_rate(step)*cost_diff
+            beta = beta - v
 
             step += 1
 
@@ -67,11 +72,11 @@ if __name__ == '__main__':
     data = real_terrain.get_data(20)
     print(data['x_train'].shape)
 
-    epochs = 30000
+    epochs = 1000
     X_test = tune.poly_design_matrix(6, data['x_train'])
 
-    for i, learning_rate in enumerate(np.logspace(-1, -0.8, 3)):
-        beta, errors = sgd(data, 20, epochs, lambda t: learning_rate, 6)
+    for i, learning_rate in enumerate(np.logspace(-7, -0.8, 5)):
+        beta, errors = sgd(data, 20, epochs, lambda t: learning_rate, 6, 0.5)
         plt.semilogy(errors, label=f"$\\gamma$={learning_rate:.3e}")
         print(f"{learning_rate}:\n Error: {metrics.MSE(data['y_train'], X_test @ beta)}\n beta: {beta}")
     
