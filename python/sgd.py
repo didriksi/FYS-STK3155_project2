46c867012740bb3c5b911f47beb58eaf18ae09b9
Didrik Sten Ingebrigtsen <didrik@eduroam-193-157-202-47.wlan.uio.no>
2020-10-23 16:52:13 +0200

    Gave learning rate functions the option of having names

--- python/sgd.py
+++ python/sgd.py
@@ -20,8 +20,9 @@ def sgd(data, mini_batches, epochs, learning_rate_iter, polynomials, momentum=0,
     epochs:     int
                 Number of epochs.
     learning_rate_iter:
-                iterable (int -> float)
-                Iterable object of functions that takes in a timestep, and returns a learning rate.
+                iterable ((int -> float), str)
+                Iterable object of a tuple of functions that takes in a timestep, and returns a learning rate,
+                and a str indication name of learning rate function.
     polynomials:int
                 Maximum degree of polynomials in design matrix
     momentum:   float
@@ -42,7 +43,7 @@ def sgd(data, mini_batches, epochs, learning_rate_iter, polynomials, momentum=0,
     """
 
     errors_index = pd.MultiIndex.from_product([
-        range(len(learning_rate_iter)),
+        [learning_rate_name for _, learning_rate_name in learning_rate_iter],
         range(initial_conditions)],
         names = ['Learning rate number', 'Initial condition number']
         )
@@ -57,7 +58,8 @@ def sgd(data, mini_batches, epochs, learning_rate_iter, polynomials, momentum=0,
     X = tune.poly_design_matrix(polynomials, data['x_train'])
 
     beta = np.random.randn(X.shape[1], initial_conditions)
-    for j, learning_rate in enumerate(learning_rate_iters): # TODO: Vectorise this loop away
+    # TODO: Vectorise this loop away. --Seems difficult as long as it's a function that might include other factors than step
+    for learning_rate_func, learning_rate_name in learning_rate_iter:
         for epoch in range(1, epochs + 1):
             print('epoch: ', epoch)
             v = np.zeros_like(beta)
@@ -66,17 +68,16 @@ def sgd(data, mini_batches, epochs, learning_rate_iter, polynomials, momentum=0,
                 k = np.random.choice(np.arange(data_size)[indexes_used == False], size=(int(data_size/mini_batches)))
                 indexes_used[k] = True
 
-                # print(X[k].shape, beta.shape)
                 y_tilde = X[k] @ beta
 
                 # TODO: Implement classes in linear_models.py in a way where this can come from it
                 cost_diff = np.dot(X[k].T, 2/(data_size/mini_batches)*(y_tilde - data['y_train'][k][:,np.newaxis])) + 2*_lambda*beta
 
-                v = momentum*v + learning_rate(step)*cost_diff
+                v = momentum*v + learning_rate_func(step)*cost_diff
                 beta = beta - v
 
                 step += 1
-            errors.loc[j][epoch] = metrics.MSE(X @ beta, data['y_train'][:,np.newaxis])
+            errors.loc[learning_rate_name][epoch] = metrics.MSE(X @ beta, data['y_train'][:,np.newaxis])
 
 
     return errors
@@ -86,16 +87,14 @@ if __name__ == '__main__':
 
     data = real_terrain.get_data(20)
 
-    epochs = 20
+    epochs = 50
     X_test = tune.poly_design_matrix(6, data['x_train'])
 
-    # TODO: Make class or some other datastructure for learning rate functions, so that it has a name?
-    learning_rate_iters = [lambda t: learning_rate for learning_rate in np.logspace(-4, -1, 4)]
-    errors = sgd(data, 20, epochs, learning_rate_iters, 6, momentum=0.5, _lambda=0.01, initial_conditions=50)
+    learning_rate_iter = [(lambda t: learning_rate, f"flat_{learning_rate}") for learning_rate in np.logspace(-3, -1, 3)]
+    errors = sgd(data, 20, epochs, learning_rate_iter, 6, momentum=0.5, _lambda=0.01, initial_conditions=50)
 
-
-    for j in range(len(learning_rate_iters)):
-        error_for_learning_rate = errors.loc[j]
+    for j, (_, learning_rate_name) in enumerate(learning_rate_iter):
+        error_for_learning_rate = errors.loc[learning_rate_name]
         min_errors = error_for_learning_rate.min(axis=0)
         max_errors = error_for_learning_rate.max(axis=0)
         best_init_condition = error_for_learning_rate[epochs].idxmin()
@@ -109,13 +108,13 @@ if __name__ == '__main__':
     plt.ylabel("MSE")
     plt.yscale("log")
 
-    modelnumber = np.array([str(i) for i in range(1, len(learning_rate_iters) + 1)])
-    colors = np.array(["C"] * len(learning_rate_iters), dtype=object) + modelnumber
-    names = np.array(["Learning rate "] * len(learning_rate_iters), dtype=object) + modelnumber
+    modelnames = np.array([f"Learning rate {learning_rate_name}" for _, learning_rate_name in learning_rate_iter])
+    modelnumbers = np.array([str(i) for i in range(1, len(learning_rate_iter) + 1)])
+    colors = np.array(["C"] * len(learning_rate_iter), dtype=object) + modelnumbers
     handler_map = {}
     for i in range(len(colors)):
         handler_map[i] = plotting.LegendObject(colors[i])
-    plt.legend(list(range(len(learning_rate_iters))), names, handler_map=handler_map)
+    plt.legend(list(range(len(learning_rate_iter))), modelnames, handler_map=handler_map)
 
     #plt.savefig("../plots/sgd_different_learning_rates")
     plt.show()
