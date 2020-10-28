6406f871ea80aca649a89d93c1fab717710f6fda
Severin <severs@uio.no>
2020-10-28 15:35:46 +0100

    Neural net implementation for SDG with bugs

--- python/neural_model.py
+++ python/neural_model.py
@@ -29,17 +29,6 @@ class Layer:
         self.a = np.zeros(height)
         self.height = height
 
-    def set_prevLayer(self, prevLayerHeight):
-        """Initialise weights after making the layer before it.
-
-        Parameters:
-        -----------
-        prevLayerHeight:
-                    int
-                    Number of neurons in previous layer.
-        """
-        self.w = np.random.normal(0, 1, (prevLayerHeight, self.height))
-
 
 class Input(Layer):
     """Input layer in neural network.
@@ -70,7 +59,6 @@ class Dense(Layer):
         super().__init__(height)
         self.z = np.zeros_like(self.a)
         self.d = np.zeros_like(self.a)
-        self.b = np.random.normal(0, 1, height)
         self.activation, self.diff_activation = activation, diff_activation
 
     def feed_forward(self, prevLayer):
@@ -81,11 +69,25 @@ class Dense(Layer):
         prevLayer:  array of floats
                     Activation values from previous layer
         """
-        self.z = np.dot(prevLayer.a, self.w)+self.b
+        self.z = np.dot(prevLayer.a, self.weights) + self.bias
         self.a = self.activation(self.z)
 
     def back_propagate(self, nextLayer):
-        self.d = np.dot(nextLayer.w, nextLayer.d.T).T * self.diff_activation(self.z)
+        self.d = np.dot(nextLayer.weights, nextLayer.d.T).T * self.diff_activation(self.z)
+
+    def set_prevLayer(self, prevLayerHeight):
+        """Initialise weights and biases after making the layer before it.
+
+        Parameters:
+        -----------
+        prevLayerHeight:
+                    int
+                    Number of neurons in previous layer.
+        """
+        self.weights = np.random.normal(0, 1, (prevLayerHeight, self.height))
+        self.bias = np.random.normal(0, 1, self.height)
+
+
 
 class Output(Dense):
     """Output layer for neural network.
@@ -129,6 +131,7 @@ class Network:
     def __init__(self, learning_rate=0.006):
         self.learning_rate = learning_rate
         self.network = []
+        self.theta = np.array([], dtype=object)
 
     def addLayer(self, layer):
         """Add a layer to network.
@@ -140,11 +143,17 @@ class Network:
         """
         self.network.append(layer)
 
+
     def compile(self):
-        """Finalises the network by initialising weights, and making the list of layers an array.
+        """Compiles the network by initialising weights, and making the list of layers an array.
+
+        Also functions as a reset to random initial conditions.
         """
+        theta = self.theta.tolist()
         for i in range(1, len(self.network)):
             self.network[i].set_prevLayer(self.network[i-1].height)
+            theta.append((self.network[i].weights, self.network[i].bias))
+        self.theta = np.array(theta, dtype=object)
         self.network = np.asarray(self.network)
 
     def feed_forward(self, input_neurons):
@@ -157,8 +166,12 @@ class Network:
                     Input values. Must have same shape as input layer.
         """
         self.network[0].a = input_neurons
-        for i in range(1,len(self.network)):
+        for i in range(1, len(self.network)):
             self.network[i].feed_forward(self.network[i-1])
+
+    def predict(self, x):
+        self.feed_forward(x)
+        return self.network[-1].a
 
     def back_propagate(self, optimal_output):
         """Propagates the error back through the network, and steps in a direction of less loss.
@@ -169,6 +182,7 @@ class Network:
                     array of floats
                     Optimal or desired output values. Must have same shape as output layer.
         """
+
         for i in reversed(range(1, len(self.network))):
             if i == len(self.network) - 1:
                 backprop_data = optimal_output
@@ -177,13 +191,59 @@ class Network:
 
             self.network[i].back_propagate(backprop_data)
 
-            self.network[i].b += np.mean(self.network[i].d, axis=0) * self.learning_rate
-            self.network[i].w += np.dot(self.network[i-1].a.T, self.network[i].d) * self.learning_rate
+            self.network[i].bias += np.mean(self.network[i].d, axis=0) * self.learning_rate
+            self.network[i].weights += np.dot(self.network[i - 1].a.T, self.network[i].d) * self.learning_rate
+
+    def backprop(self, input, optimal_output):
+        """
+
+        Args:
+            input: input data
+            optimal_output: target data
+
+        Returns:
+            nabla_b: gradient of bias
+            nabla_w: gradient of weights
+        """
+
+        nabla_b = []
+        nabla_w = []
+        self.feed_forward(input)
+        for i in reversed(range(1, len(self.network))):
+            if i == len(self.network) - 1:
+                backprop_data = optimal_output
+            else:
+                backprop_data = self.network[i+1]
+            self.network[i].back_propagate(backprop_data)
+
+            nabla_b.append(np.mean(self.network[i].d, axis=0))
+            nabla_w.append(np.dot(self.network[i - 1].a.T, self.network[i].d))
+
+        return nabla_b, nabla_w
+
+    def update_parameters(self, mini_batch_x):
+        nabla_b = []
+        nabla_w = []
+        for i in reversed(range(1, len(self.network))):
+            nabla_b.append(np.zeros_like(self.network[i].bias))
+            nabla_w.append(np.zeros_like(self.network[i].weights))
+
+        for x in mini_batch_x:
+            delta_nabla_b, delta_nabla_w = self.backprop(x[0], x[1])
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        nabla_b.reverse()
+        nabla_w.reverse()
+        for i in reversed(range(1, len(self.network))):
+            self.network[i].weights += nabla_w[i-1]*self.learning_rate
+            self.network[i].bias += nabla_b[i-1]*self.learning_rate
 
     def __str__(self):
         """Returns a printable string with all the networks biases and weights.
         """
         string = "Neural net\n"
         for i in range(1, len(self.network)):
-            string += f"Layer {i}\nBiases: {self.network[i].b}\nWeights: {self.network[i].w}\n"
+            string += f"Layer {i}\n   Biases: {self.network[i].bias.shape}\n   Weights: {self.network[i].weights.shape}\n"
+            #string += f"delta_b = {self.network[i].delta_b.shape}\n delta_w: {self.network[i].delta_w.shape}"
         return string
