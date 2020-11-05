Fixed some small bugs with broadcasting

python/linear_models.py
@@ -144,7 +144,7 @@ class LinearRegression:
                     Differentiated MSE.
 
         """
-        return np.dot(X.T, 2/X.shape[0]*(y_tilde - y[:,np.newaxis])) + 2*self._lambda*self.beta
+        return np.dot(X.T, 2/X.shape[0]*(y_tilde - y)) + 2*self._lambda*self.beta
 
     def update_parameters(self, X, y, y_tilde):
         """Performs one step of gradient descent.

python/sgd.py
@@ -357,7 +357,10 @@ if __name__ == '__main__':
         models = [model for models, _ in subplots for model in models]
         sgd_betas = np.array([model.beta for model in models])
 
-        optimal_betas = np.array([model.fit(X_train, data['y_train']) for model in models ])
+        sgd_errors = np.mean([metrics.MSE(model.predict(X_validate), data['y_validate'][:,np.newaxis]) for model in models])
+        optimal_betas = np.array([model.fit(X_train, data['y_train']) for model in models])
+        optimal_errors = np.mean([metrics.MSE(model.predict(X_validate), data['y_validate'][:,np.newaxis]) for model in models])
+        print(f"Mean SGD MSE after convergence: {sgd_errors}. Analytical MSE: {optimal_errors}")
         sgd_beta_intervals = np.array([np.min(sgd_betas, axis=(0,2)), np.mean(sgd_betas, axis=(0,2)), np.max(sgd_betas, axis=(0,2))])
         optimal_beta_intervals = np.array([np.min(optimal_betas, axis=0), np.mean(optimal_betas, axis=0), np.max(optimal_betas, axis=0)])
 