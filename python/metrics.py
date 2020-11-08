fe002753d4bcff90866aef5a44bf4f1617c669c5
Severin <severs@uio.no>
2020-11-08 13:49:09 +0100

    Implemented cross entropy cost function

--- python/metrics.py
+++ python/metrics.py
@@ -2,8 +2,10 @@ import numpy as np
 
 MSE = lambda y, y_tilde: np.mean(np.square(y - y_tilde), axis=0)
 
-R_2 = lambda y, y_tilde: 1 - np.sum(np.square(y - y_tilde))/np.sum(np.square(y - np.mean(y)))
+R_2 = lambda y, y_tilde: 1 - np.sum(np.square(y - y_tilde)) / np.sum(np.square(y - np.mean(y)))
 
-model_variance = lambda y_tilde: np.mean((y_tilde - np.mean(y_tilde, axis=0))**2)
+model_variance = lambda y_tilde: np.mean((y_tilde - np.mean(y_tilde, axis=0)) ** 2)
 
-model_bias = lambda y, y_tilde: np.mean((y - np.mean(y_tilde, axis=0))**2)
+model_bias = lambda y, y_tilde: np.mean((y - np.mean(y_tilde, axis=0)) ** 2)
+
+cross_entropy = lambda y, y_tilde: np.mean(y * np.log(y_tilde) + (1 - y) * np.log(1 - y_tilde))
