cd3d2cd52e48001fed9c0e1e31ca2246e5d9d122
Didrik Sten Ingebrigtsen <didrik@eduroam-193-157-242-252.wlan.uio.no>
2020-11-08 14:18:35 +0100

    Added docstrings to metrics

--- python/metrics.py
+++ python/metrics.py
@@ -1,11 +1,19 @@
 import numpy as np
 
-MSE = lambda y, y_tilde: np.mean(np.square(y - y_tilde), axis=0)
+def MSE(y, y_tilde):
+    """MSE"""
+    return np.mean(np.square(y - y_tilde), axis=0)
 
-R_2 = lambda y, y_tilde: 1 - np.sum(np.square(y - y_tilde)) / np.sum(np.square(y - np.mean(y)))
+def R_2(y, y_tilde):
+    """R2"""
+    return 1 - np.sum(np.square(y - y_tilde)) / np.sum(np.square(y - np.mean(y)))
 
 model_variance = lambda y_tilde: np.mean((y_tilde - np.mean(y_tilde, axis=0)) ** 2)
 
 model_bias = lambda y, y_tilde: np.mean((y - np.mean(y_tilde, axis=0)) ** 2)
 
-cross_entropy = lambda y, y_tilde: np.mean(y * np.log(y_tilde) + (1 - y) * np.log(1 - y_tilde))
+def cross_entropy(y, y_tilde):
+    """Cross entropy"""
+    return - np.mean(y * np.log(y_tilde) + (1 - y) * np.log(1 - y_tilde))
+
+#cross_entropy = lambda y, y_tilde: np.mean(y * np.log(y_tilde) + (1 - y) * np.log(1 - y_tilde))
